7409550657df8ba24a0285f01bd368a0604525a2
nevilleomangi <31406314+nevilleomangi@users.noreply.github.com>
2021-11-03 13:37:43 +0300

    add tests

--- vigenere/vinegere_test.go
+++ vigenere/vinegere_test.go
@@ -1,14 +1,34 @@
 package vigenere_test
 
-// m := "lorem ipsum dolor sit amet"
-// k := "ugali"
+import (
+	"testing"
 
-// fmt.Println(m)
-// c := Encrypt(k, m)
-// fmt.Println(c)
-// d := Decrypt(k, c)
-// fmt.Println(d)
-// fmt.Println([]byte(d))
+	"github.com/nevilleomangi/vigenere-cypher/vigenere"
+)
 
-// lorem ipsum dolor sit amet
-// furpu cvsfu xulzz mot luyz
+func TestVigenere(t *testing.T) {
+	key := "ugali"
+
+	cases := []struct {
+		test, message, cipher string
+	}{
+		{"special characters", "lorem!#", "furpu!#"},
+		{"lowercase ASCII", "lorem ipsum dolor sit amet", "furpu cvsfu xulzz mot luyz"},
+		{"uppercase ASCII", "LOREM IPSUM DOLOR SIT AMET", "FURPU CVSFU XULZZ MOT LUYZ"},
+	}
+
+	for _, c := range cases {
+		t.Run("Encrypts "+c.test, func(t *testing.T) {
+			got := vigenere.Encrypt(key, c.message)
+			if got != c.cipher {
+				t.Fatalf("\nMessage:%q\n Got:%q\n Want:%q\n", c.message, got, c.cipher)
+			}
+		})
+		t.Run("Decrypts "+c.test, func(t *testing.T) {
+			got := vigenere.Decrypt(key, c.cipher)
+			if got != c.message {
+				t.Fatalf("\nCipher:%q\n Got:%q\n Want:%q\n", c.cipher, got, c.message)
+			}
+		})
+	}
+}
